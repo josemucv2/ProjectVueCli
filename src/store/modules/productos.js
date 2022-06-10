dc41504d25d954308ce9c85d2b936bdf6f9cb17e
josemucv2 <martinezcaceresjosem@gmail.com>
2022-06-10 00:18:47 -0400

    prueba

--- src/store/modules/productos.js
+++ src/store/modules/productos.js
@@ -4,7 +4,7 @@ export default {
     namespaced: true,
     state: {
         listaProductos: [],
-        Mensaje: 'Haciendo cambios en ramas externas de git',
+        Mensaje: 'Viendo los cambios en la rama de git Login',
     },
     getters: {
         getlistaProductos: state => (length) => {
